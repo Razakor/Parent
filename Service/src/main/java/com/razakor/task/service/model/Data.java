cef6ffad4ec5774762b17f670e70beb92d7d89c0
Razakor <m.shevchuk.o@gmail.com>
2019-10-16 14:47:01 +0300

    refactoring

--- Service/src/main/java/com/razakor/task/service/model/Data.java
+++
@@ -1,9 +0,0 @@
-package com.razakor.task.service.model;
-
-import com.razakor.task.documents.Trolleybuses;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Data {
-    public static List<Trolleybuses> trolleybuses = new ArrayList<>();
-}
